5ccbbce41ad7eb5e367fa5c258b0e532f925682f
Fatih Deniz <deniz.fatih@hotmail.de>
2021-11-23 04:23:26 +0100

    3- Rate-Limit Library

---
+++ RateLimit.API/Controllers/CategoriesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RateLimit.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCategory()
+        {
+            return Ok(new {Id = 1, Category = "Kitap"});
+        }
+
+    }
+}
--- RateLimit.API/Startup.cs
+++ RateLimit.API/Startup.cs
@@ -1,16 +1,11 @@
+using AspNetCoreRateLimit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RateLimit.API
 {
@@ -26,9 +21,36 @@ namespace RateLimit.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //RATE LIMIT AYARLARI\\
+            services.AddOptions();
+            services.AddMemoryCache(); //Dakika yapilan request sayilarini ram'de tutar
 
+            //IP-RATE LIMIT
+            //services.Configure<IpRateLimitOptions>(Configuration.GetSection("IpRateLimiting")); //appsettings.json icerisinde verecegimiz baglanti limitleri
+            //services.Configure<IpRateLimitPolicies>(Configuration.GetSection("IpRateLimitPolicies")); //appsettings.json icerisinde verecegimiz kurallar
 
+            //services.AddSingleton<IIpPolicyStore, MemoryCacheClientPolicyStore>(); //Eski sürümlerde gecerli - Uygulamamizda tek instance ayaga kalkiyorsa MemoryCacheClientPolicyStore kullanilir. Birden fazla instance ayaga kalkiyorsa (örnek: Docker da 5 container ayaga kaldirirsak, merkezi bir yerde sayac tutulmasi gerekir. Bunun icinde DistributedCacheClientPolicyStore kullanmamiz gerekir. REDIS gibi.)
 
+            //CLIENT RATE LIMIT
+            services.Configure<ClientRateLimitOptions>(Configuration.GetSection("ClientRateLimiting")); //appsettings.json icerisinde verecegimiz baglanti limitleri
+            services.Configure<ClientRateLimitPolicies>(Configuration.GetSection("ClientRateLimitPolicies")); //appsettings.json icerisinde verecegimiz kurallar
+            services.AddInMemoryRateLimiting(); //Yeni sürüm icin gecerli
+
+            //services.AddSingleton<IClientPolicyStore, MemoryCacheClientPolicyStore>(); //Eski sürümlerde gecerli
+
+            //services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>(); //Eski sürümlerde gecerli - yukaridaki ile ayni
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>(); // Defaultta kapali gelen bir servis. Ihtiyac varsa bu özellik acilir.Middleware katmanlar icin olusturulur. Gelen Request icerisindeki header bilgisi, IP adresi gibi bilgileri ayni zamanda http metot tipi (get,put,post) bunlari ayri ayri okumaya yarar.
+
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+            /*Middleware Nedir
+             *
+             * Güvenlik katmanlarinin yani sira, gerekli ise yeniden ele alinmasi, farkli veri kaynaklarindan gelen verilerin birlestirilmesi, bir veya daha fazla veri kaynagina iletilmesi, ayristirilmasi gibi pek cok tekrarlanan islemler söz konusu olabilir.
+             *
+             * Middleware (ara yazilim) tekrar eden islemleri üstlenen, amaca yönelik görevleri gerceklestiren cözümlerden biridir.
+             */
+            //RATE LIMIT AYARLARI\\
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -46,6 +68,12 @@ namespace RateLimit.API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RateLimit.API v1"));
             }
+
+            //Ip-Rate Limit
+            //app.UseIpRateLimiting(); // IpRateLimit middleware yukarida tanimlanir,buraya eklenir. 
+
+            //Client-Rate Limit
+            app.UseClientRateLimiting(); // ClientRateLimit middleware yukarida tanimlanir,buraya eklenir. 
 
             app.UseHttpsRedirection();
 
